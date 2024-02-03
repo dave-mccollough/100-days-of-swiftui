d450aaa24e27cf0707b117bf3624ffcd700da36d
Dave McCollough <davemccollough@gmail.com>
2024-02-02 21:08:13 -0700

    Checkpoint 1

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 - [Day 1 – Variables, constants, strings, and numbers](days-1-15/day-01.swift)
 - [Day 2 – Booleans and string interpolation](days-1-15/day-02.swift)
+- [Checkpoint 1](days-1-15/checkpoint-1.swift)
---
+++ days-1-15/checkpoint-1.swift
@@ -0,0 +1,5 @@
+// Convert temperatures from Celsius to Fahrenheit.
+
+let celsiusTemperature = 29.0
+let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32
+print("The temperature in fahrenheit is \(String(fahrenheitTemperature))°")
