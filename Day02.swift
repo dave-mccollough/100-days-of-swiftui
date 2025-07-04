d3ec03cd537a9bbe5264bb975f7ec7742dde5114
Dave McCollough <davemccollough@gmail.com>
2025-07-04 14:40:00 -0600

    Day 2

---
+++ Day02.swift
@@ -0,0 +1,38 @@
+// Booleans
+
+// Stores truth oor false
+
+let goodDogs = true
+let gameOver = false
+
+// Assign boolean values as loiing as they are true or false
+
+let isMultiple = 120.isMultiple(of: 3)
+
+// String Concatenation
+
+// Option One
+let first = "Hello "
+let second = "World"
+let statement = first + second
+
+// Optoin 2 - String Interpolation
+
+let bike = "Yeti"
+let trail = "Colorado Trail"
+let phrase = "I'm going to ride my \(bike) on the \(trai) today!"
+
+// Change number to string
+let rideTime = 5
+let message = "we are going to ride bikes at \(string(rideTime))pm"
+
+// CHeckpoint 1
+
+// Your goal is to write a Swift playground that:
+
+// Creates a constant holding any temperature in Celsius.
+let celsiusTemp = 32
+// Converts it to Fahrenheit by multiplying by 9, dividing by 5, then adding 32.
+let farenheitTemp = (celsiusTemp * 9 / 5) + 32
+// Prints the result for the user, showing both the Celsius and Fahrenheit values.
+print("Celsius temperature is \(celsiusTemp)°C and farenheit temperature is \(farenheitTemp)°F")
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 100-days-of-swiftui
 
 - [Day 1 - Variables, constants, strings, and numbers](Day01.swift)
+- [Day 2 – Booleans, string interpolation, and checkpoint 1](Day02.swift)
